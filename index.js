cd6f914371a86256b619f3190bbb6e811e72933e
Andrew Owens <andrrowens@gmail.com>
2022-11-13 14:50:39 -0700

    commit tree 238d22d0167affe4af9e74aa87e2fa913e59f437

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching() { drivers.filter(driver => driver === "Bobby");
+};
+
+console.log(findMatching);
+
+
+
+//findMatching(drivers, "Bobby")
+//};
+
+    //function findMatching(drivers, name){
+       // return drivers.filters
+
+
+
+//console.log(findMatching(drivers, "Bobby"));
+
+
+
+
+
+
+    //function findMatching(driver) {
+    //    return drivers.filter(driver) = ["Bobby];
+   // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
